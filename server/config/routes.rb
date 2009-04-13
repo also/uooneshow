041eb8c9bce6441d1aa165c47b617f856fccf2c3
Ryan Berdeen <ryan@ryanberdeen.com>
2009-04-12 17:04:45 -0700

    using new message page as root

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :snapshots
   map.resources :messages
 
+  map.root :controller => 'messages', :action => :new
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
