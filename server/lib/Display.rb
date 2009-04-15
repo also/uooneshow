8d20c91a2838030a0803064e37ed16e3fe5aea7f
Ryan Berdeen <ryan@ryanberdeen.com>
2009-04-14 22:26:37 -0700

    catching connection refused errors

--- server/lib/Display.rb
+++ server/lib/Display.rb
@@ -2,9 +2,13 @@ require 'socket'
 
 class Display
   def self.send_message(message)
+    begin
     TCPSocket.open('localhost', 1843) do |s|
       s.print "hi\0"
       s.print "#{message}\0"
+    end
+    rescue Errno::ECONNREFUSED => ex
+      # TODO log error
     end
   end
 end
