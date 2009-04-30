7012a7866a1d4542fca52849754fa3aaaf33d43e
Ryan Berdeen <ryan@ryanberdeen.com>
2009-04-29 21:17:40 -0700

    notifying display of new snapshots

--- server/app/controllers/feed_items_controller.rb
+++ server/app/controllers/feed_items_controller.rb
@@ -4,6 +4,7 @@ class FeedItemsController < ApplicationController
     scope = scope.since_id(params[:since_id]) if params[:since_id]
 
     @feed_items = scope.all :order => 'created_at DESC', :limit => 15
+    @feed_items.reverse
     respond_to do |format|
       format.html
       format.json { render :json => {:feed_items => @feed_items, :max_id => FeedItem.maximum(:id)} }
@@ -17,6 +18,7 @@ class FeedItemsController < ApplicationController
     attributes = params[:feed_item]
     attributes[:source] = 'snapshot'
     if @feed_item = FeedItem.create(attributes)
+      Display.send_message('display-feed new_message ' + @feed_item.to_json)
       redirect_to new_feed_item_url
     else
       render :text => 'nope!'
