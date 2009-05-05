07fed98d64d19f0cf31a42603eec921c19402804
Ryan Berdeen <ryan@ryanberdeen.com>
2009-05-05 00:57:02 -0400

    added simple form to create reel items

--- server/app/controllers/reel_items_controller.rb
+++ server/app/controllers/reel_items_controller.rb
@@ -3,7 +3,20 @@ class ReelItemsController < ApplicationController
     @reel_items = ReelItem.all :order => 'position ASC, created_at DESC'
 
     respond_to do |format|
+      format.html
       format.json { render :json => {:reel_items => @reel_items} }
+    end
+  end
+
+  def new
+  end
+
+  def create
+    @reel_item = ReelItem.create(params[:reel_item])
+    if @reel_item.save
+      redirect_to reel_items_url
+    else
+      render :text => 'nope!'
     end
   end
 end
