78f0676913689c5764c7f7bef9b731969512f36f
Ryan Berdeen <ryan@ryanberdeen.com>
2009-05-21 19:33:02 -0700

    fixed reel item json

--- server/app/models/reel_item.rb
+++ server/app/models/reel_item.rb
@@ -1,3 +1,8 @@
 class ReelItem < ActiveRecord::Base
   has_many :parts, :class_name => 'ReelItemPart'
+
+  def to_json(options)
+    # TODO add to existing options
+    super(:include => {:parts => {:only => [:media_type, :media_url, :position]}})
+  end
 end
